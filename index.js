46a704578fa56376f967ab63a66abf121b03921b
Ellite <46927093+nekoellie@users.noreply.github.com>
2020-04-07 14:11:35 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const token = process.env.token;
+
+
+bot.login(token);
+
+bot.on('ready', () =>{
+    console.log("We're rolling baby");
+})
+
+
+bot.on('message', message => {
+	if (message.content.includes('map')) {
+        message.react('🗺️');
+    }
+    else {
+        if (message.content.includes('mapping')) {
+            message.react('🗺️');
+        }
+    else {
+        if (message.content.includes('mapped')) {
+            message.react('🗺️');
+        }
+    else {
+        if (message.content.includes('mapper')) {
+            message.react('🗺️');
+        }
+    }
+    }
+    }
+});
