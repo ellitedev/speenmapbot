3dafa9fd4bd83da2b21214006b65c5ba86800e08
jy1263 <50583248+jy1263@users.noreply.github.com>
2020-04-09 22:29:10 +1000

    made basic filter

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ bot.on('message', message => {
         }
 
 
-
-    else if (lowerCaseMessageContent.startsWith('!search ')) {
+    else if (lowerCaseMessageContent.startsWith('!search ') && message.channel.id == '638817716634910722') {
+        console.log(message.channel.id);
         message.react('🔍');
         let searchterm = message.content.slice(8)
             api.search(searchterm).then(function(songArray) {
