bbbccaf1946790ee364fa537922d54f10c0e1257
Ellite <46927093+nekoellie@users.noreply.github.com>
2020-05-14 21:14:12 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -3,7 +3,7 @@ const Discord = require("index.js")
 module.exports.run = async (bot, message, args) => {
     console.log("works");
     const channel = bot.channels.cache.get('697732663045259334');
-    channel.send("We're back up and speening!");
+    channel.send("works");
 }
 
 module.exports.help = {
