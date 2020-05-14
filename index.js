c88887d6471fc71e9a874558877caef227a6fe56
Ellite <46927093+nekoellie@users.noreply.github.com>
2020-05-14 22:27:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+bot.login(token);
 const botconfig = require("./botconfig.json");
 const bot = new Discord.Client();
-bot.login(token);
 const token = process.env.token;
 const SSAPI = require('./assets/js/module.api.js');
 const emojiCharacters = require('./assets/js/module.emojis.js');
