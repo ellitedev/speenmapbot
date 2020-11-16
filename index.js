1885fa157a8801cfb3d325122cd39ad5807ae6fd
Andreas Heimann <andreas@iklikla.de>
2020-11-16 12:38:39 +0100

    removed reaction emojis

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ let api = new SSAPI();
 const fs = require("fs");
 const { prefix } = require("./botconfig.json");
 
-// TODO: ELLITE, change this to the role name of limitedmode, else weebs will be confused
-const limitedRoleName = "limiter";
-
 bot.commands = new Discord.Collection();
 
 const commandFiles = fs
@@ -57,115 +54,8 @@ bot.on("message", (message) => {
 });
 
 bot.on("message", (message) => {
-  // if (!message.content.startsWith(prefix) || message.author.bot) return;
-  // const args = message.content.slice(prefix.length).split(/ +/);
-  // const command = args.shift().toLowerCase();
-
-  // if (command === 'ping') {
-  // 	message.channel.send('Pong.');
-  // } else if (command === 'beep') {
-  // 	message.channel.send('Boop.');
-  // }
-
-  // Above is old command handling code, this is a make shift merging of the two that (hopefully) works
-
   // TODO: ORGANISE ALL THIS, the tech debt is gonna be huge if we continue like this.
-
-  // limitedMode disables everything that is not fired manually (Primarily meme roles)
-  // Something tells me this should be booleanified using !!, that'll come later though. (Done now)
-  let limitedBoolCalc;
-  try {
-    limitedBoolCalc = message.member.roles.cache.find(
-      (r) => r.name === limitedRoleName
-    );
-  } catch (err) {
-    limitedBoolCalc = false;
-    console.log("error: " + err);
-  }
-
-  let limitedMode = Boolean(limitedBoolCalc);
-
   let lowerCaseMessageContent = message.content.toLowerCase();
-
-  if (!limitedMode) {
-    if (
-      lowerCaseMessageContent.includes(
-        "map",
-        "mapping",
-        "mapped",
-        "mapper",
-        "mappers"
-      )
-    ) {
-      message.react("🗺️");
-    }
-
-    if (
-      lowerCaseMessageContent.includes(
-        "chart",
-        "charting",
-        "charted",
-        "charter",
-        "charters"
-      )
-    ) {
-      message.react("📈");
-    }
-
-    if (lowerCaseMessageContent.includes("speen")) {
-      message.react("695440682952687656");
-      message.react("695440704809336942");
-      message.react("695440945306533939");
-    }
-
-    if (lowerCaseMessageContent.includes("spleen")) {
-      message.react("695440682952687656");
-      message.react(emojiCharacters.l);
-      message.react("695440704809336942");
-      message.react("695440945306533939");
-    }
-
-    if (lowerCaseMessageContent.includes("mapy")) {
-      message.react("699274566849265756");
-    }
-
-    let metalman = [
-      "metalman",
-      "metalman20",
-      "guitarman",
-      "metal",
-      "guitar",
-      "<@!105429810158784512>",
-    ];
-    if (metalman.some((el) => lowerCaseMessageContent.includes(el))) {
-      message.react("704090735707685067");
-    }
-
-    let way = ["way", "theway", "honestly"];
-    if (way.some((el) => lowerCaseMessageContent.includes(el))) {
-      message.react("745814454901407816");
-    }
-
-    let trex = ["trex"];
-    if (trex.some((el) => lowerCaseMessageContent.includes(el))) {
-      message.react("745814455677354054");
-    }
-
-    let ambi = ["ambi", "ambulance"];
-    if (ambi.some((el) => lowerCaseMessageContent.includes(el))) {
-      message.react("745814454788292680");
-    }
-
-    let kali = ["blade", "kali"];
-    if (kali.some((el) => lowerCaseMessageContent.includes(el))) {
-      message.react("745817406265360384");
-    }
-
-    if (lowerCaseMessageContent.includes("trump")) {
-      message.react("704087779193258005");
-      message.react("745814455677354054");
-    }
-  }
 
   let faq = [
     "download custom",
